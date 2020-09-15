378ff502a049fe776fd725b9d0104836864aa71e
Cristian <cristianpalacios95@gmail.com>
2020-09-15 18:00:09 -0300

    Creacion de la clase Mascota y del package domain

---
+++ mascotas/src/main/java/ar/edu/undec/mascotas/domain/Mascota.java
@@ -0,0 +1,10 @@
+package ar.edu.undec.mascotas.domain;
+
+import java.time.LocalDate;
+
+public class Mascota {
+
+    public static Mascota getInstance(String nombre, String raza, LocalDate fechaNac) {
+        return null;
+    }
+}
--- mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/CrearMascotaUseCaseUnitTest.java
+++ mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/CrearMascotaUseCaseUnitTest.java
@@ -1,8 +1,11 @@
 package ar.edu.undec.mascotas.usecaseunittest;
 
 import Mockito.MockitoExtension;
+import ar.edu.undec.mascotas.domain.Mascota;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.time.LocalDate;
 
 @ExtendWith({MockitoExtension.class})
 
@@ -10,6 +13,8 @@ public class CrearMascotaUseCaseUnitTest {
 
     @Test
     void crearMascota_mascotaNoExiste_crearMascota(){
+
+        Mascota mascota01= Mascota.getInstance("Ringo", "Labrador", LocalDate.of(2005, 11, 12));
 
     }
 

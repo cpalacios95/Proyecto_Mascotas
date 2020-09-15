a8808befae9ab5cdad60d6fdd1d166251bd22a3c
Cristian <cristianpalacios95@gmail.com>
2020-09-15 17:52:54 -0300

    Creacion de la clase CrearMascotaUseCaseUnitTest y del package usecaseunittest

---
+++ mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/CrearMascotaUseCaseUnitTest.java
@@ -0,0 +1,16 @@
+package ar.edu.undec.mascotas.usecaseunittest;
+
+import Mockito.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith({MockitoExtension.class})
+
+public class CrearMascotaUseCaseUnitTest {
+
+    @Test
+    void crearMascota_mascotaNoExiste_crearMascota(){
+
+    }
+
+}

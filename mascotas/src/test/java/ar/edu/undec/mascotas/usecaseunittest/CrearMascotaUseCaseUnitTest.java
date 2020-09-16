ca6336aeefecbc64aa63595f201468ec211dd019
Cristian <cristianpalacios95@gmail.com>
2020-09-16 14:57:52 -0300

    Creacion del test crearMascota_mascotaExiste_noCrearMascota()

--- mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/CrearMascotaUseCaseUnitTest.java
+++ mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/CrearMascotaUseCaseUnitTest.java
@@ -4,13 +4,13 @@ import Mockito.MockitoExtension;
 import ar.edu.undec.mascotas.domain.Mascota;
 import ar.edu.undec.mascotas.repository.ICrearMascotaRepository;
 import ar.edu.undec.mascotas.usecase.CrearMascotaUseCase;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
@@ -30,7 +30,21 @@ public class CrearMascotaUseCaseUnitTest {
         when(crearMascotaRepository.existeMascota(mascota01.getNombre())).thenReturn(false);
         when(crearMascotaRepository.guardarMascota(mascota01)).thenReturn(true);
 
-        Assertions.assertTrue(crearMascotaUseCase.crearMascota(mascota01));
+        assertTrue(crearMascotaUseCase.crearMascota(mascota01));
+
+    }
+
+    @Test
+    void crearMascota_mascotaExiste_noCrearMascota(){
+
+        Mascota mascota01 = Mascota.getInstance("Tomi", "Labrador", LocalDate.of(200,12,1));
+
+        CrearMascotaUseCase mascotaUseCase = new CrearMascotaUseCase(crearMascotaRepository);
+
+        when(crearMascotaRepository.existeMascota(mascota01.getNombre())).thenReturn(true);
+        when(crearMascotaRepository.guardarMascota(mascota01)).thenReturn(false);
+
+        assertFalse(mascotaUseCase.crearMascota(mascota01));
 
     }
 

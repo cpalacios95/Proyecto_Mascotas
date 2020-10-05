fc85f5a3370d6062f69ea3833ba87b2d09346ce8
Cristian <cristianpalacios95@gmail.com>
2020-10-05 20:35:42 -0300

    Conexion a db en application.properties

--- mascotas/src/main/java/ar/edu/undec/mascotas/persistence/entity/MascotaEntity.java
+++ mascotas/src/main/java/ar/edu/undec/mascotas/persistence/entity/MascotaEntity.java
@@ -1,15 +1,58 @@
 package ar.edu.undec.mascotas.persistence.entity;
 
-import ar.edu.undec.mascotas.domain.Cliente;
-
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity(name = "mascotas")
+@SequenceGenerator(name = "mascota_id_gen", initialValue = 1, sequenceName = "mascota_id_gen", allocationSize = 1)
 public class MascotaEntity {
 
+    @Id
+    @Column(name = "idmascota")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mascota_id_gen")
+    private int idMascota;
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "fechanac")
     private LocalDate fechaNac;
+
+    @Column(name = "raza")
     private String raza;
-    private Cliente duenio;
 
+    public MascotaEntity(){
+    }
 
+    public int getIdmascota() {
+        return idMascota;
+    }
+
+    public void setIdmascota(int idmascota) {
+        this.idMascota = idmascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
 }

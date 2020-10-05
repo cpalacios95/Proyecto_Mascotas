f794668d080491cc5dce6a34526de393ad021e2e
Cristian <cristianpalacios95@gmail.com>
2020-10-05 20:15:10 -0300

    Creacion del package crud y entity, y de la clase MascotaEntity

---
+++ mascotas/src/main/java/ar/edu/undec/mascotas/persistence/entity/MascotaEntity.java
@@ -0,0 +1,15 @@
+package ar.edu.undec.mascotas.persistence.entity;
+
+import ar.edu.undec.mascotas.domain.Cliente;
+
+import java.time.LocalDate;
+
+public class MascotaEntity {
+
+    private String nombre;
+    private LocalDate fechaNac;
+    private String raza;
+    private Cliente duenio;
+
+
+}
